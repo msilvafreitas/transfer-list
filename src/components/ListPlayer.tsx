3c68aa14887f5c1af15e1b32213f772772d62826
matheus <freitas1matheus@gmail.com>
2022-09-18 23:39:31 -0300

    list player responsive

--- src/components/ListPlayer.tsx
+++ src/components/ListPlayer.tsx
@@ -3,9 +3,9 @@ import * as Dialog from '@radix-ui/react-dialog'
 
 export function ListPlayer()  {
     return (
-        <div className='pt-1 self-stretch bg-tol-gradient rounded-lg mt-8 overflow-hidden'>
-            <div className='bg-[#2A2634] px-6 py-6 flex justify-between items-center'>
-                <div>
+        <div className='pt-1 mx-4 self-stretch bg-tol-gradient rounded-lg mt-8 overflow-hidden'>
+            <div className='bg-[#2A2634] px-6 py-6 flex flex-col md:flex-row justify-between items-center'>
+                <div className="pb-4">
                     <strong className='text-2xl text-white font-black block'>Want to sell a player?</strong>
                     <span className='text-zinc-400 block'>Publish your ad!</span>
                 </div>
